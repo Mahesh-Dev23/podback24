2e12e6224738a62c2f309f23ed2462750d5ca6ff
Mahesh-Dev23 <nomahesh@gmail.com>
2024-01-16 20:08:25 +0530

    import changed to require

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import express from 'express'
-import cors from 'cors'
-import dotEnv from 'dotEnv'
+const express = require('express') 
+const cors = require('cors')
+const dotEnv = require('dotEnv') 
 
 dotEnv.config()
 
