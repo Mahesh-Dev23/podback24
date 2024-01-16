78837e2ef0de977cfdcc66f79414633479ef685d
Mahesh-Dev23 <nomahesh@gmail.com>
2024-01-16 17:06:09 +0530

    first backend 24 added

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from 'express'
+import cors from 'cors'
+import dotEnv from 'dotEnv'
+
+dotEnv.config()
+
+const app = express()
+
+app.use(cors())
+
+app.get('/', (req, res) => {
+    res.send('Hello World')
+})
+
+app.get('/api/user', (req, res) => {
+    res.json({body: `Welcome ${Date.now()}`})
+})
+
+if( process.env.API_PORT){
+
+    app.listen( process.env.API_PORT, console.log(`POD Backend App is running on port ${process.env.API_PORT}`))
+}
+
+
+export default app
