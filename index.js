0f638f66bde1f96a391419f5677ae08d450fa339
Mahesh-Dev23 <nomahesh@gmail.com>
2024-01-16 20:28:00 +0530

    cors added

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
+
+app.use(cors())
 app.get('/',  (req, res) => {
   res.send('Hello World!')
 })
 
-app.get('/api/user', (req, res) => {
+app.get('/api/users', (req, res) => {
     res.json({body: `Welcome ${Date.now()}`})
 })
 
-app.listen(3000, console.log(`The app is running.`))
+app.listen(5000, console.log(`The app is running.`))
