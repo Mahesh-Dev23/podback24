8bb0881d6f9c58f89156489b5fa94b9a74306611
Mahesh-Dev23 <nomahesh@gmail.com>
2024-01-16 20:16:24 +0530

    index js changed to basic

--- index.js
+++ index.js
@@ -1,26 +1,8 @@
-const express = require('express') 
-const cors = require('cors')
-const dotEnv = require('dotEnv') 
-
-dotEnv.config()
-
+const express = require('express')
 const app = express()
 
-app.use(cors())
-
-app.get('/', (req, res) => {
-    res.send('Hello World')
+app.get('/', function (req, res) {
+  res.send('Hello World')
 })
 
-app.get('/api/user', (req, res) => {
-    res.json({body: `Welcome ${Date.now()}`})
-})
-
-// if( process.env.API_PORT){
-
-    app.listen( process.env.API_PORT, () => console.log(`POD Backend App is running on port ${process.env.API_PORT}`))
-// }
-
-// git remote add origin https://github.com/Mahesh-Dev23/podback24.git
-
-// export default app
+app.listen(3000, console.log(`The app is running.`))
